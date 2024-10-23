4a043897de3529e0cec9a237c3125ffafc6908d5
itsjustcc3 <nassy.hocine@gmail.com>
2024-10-23 09:15:34 -0400

    magic find fix

--- magic find.py
+++ magic find.py
@@ -1,20 +1,26 @@
+import sys
+while True:
+    chance = float(input("what is the original dropchance "))
+    magicfind = float(input("what is your magic find just the number"))
+    chance = chance * (1+(magicfind/100)/100)
+
+    print (f"""
+
+        =====================================
+        |                                   |
+        |                                   |
+        |      Chance: {chance:.02}                  |
+        |                                   |
+        |                                   |
+        =====================================
 
 
-chance = float(input("what is the original dropchance "))
-magicfind = float(input("what is your magic find just the number"))
-chance = chance * (1+(magicfind/100)/100)
 
-print (f"""
-
-       =====================================
-       |                                   |
-       |                                   |
-       |      Chance: {chance:.02}                  |
-       |                                   |
-       |                                   |
-       =====================================
-
-
-
-    """)
+        """)
+    choice = input("do you want another calculation y/n ")
+    if choice == n:
+        print("Stopping....")
+        sys.exit(0)
+    else:
+        print("Ok running program again...")
 
