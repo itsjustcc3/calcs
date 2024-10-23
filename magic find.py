6c076d8d2dc85f954eec774e05eb573ff91c770c
itsjustcc3 <nassy.hocine@gmail.com>
2024-10-23 08:50:56 -0400

    Create magic find.py

---
+++ magic find.py
@@ -0,0 +1,6 @@
+
+
+chance = float(input("what is the original dropchance"))
+magicfind = float(input("what is your magic find just the number"))
+chance = chance * (1+(magicfind/100))
+print (chance)
